95bb330cc51354aa4a09469bc42442dac225b41d
dsm <50470310+dsmuix@users.noreply.github.com>
2020-06-22 09:44:44 -0300

    Added clear function

--- hasher.py
+++ hasher.py
@@ -5,23 +5,26 @@ import time
 
 dots = '*****************************************************'
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def welcome():
-    os.system('cls')
+    clear()
     print(dots)
     print('[!] Welcome to Hasher 1.0')
     print('[!] A base85+sha384 encoder')
-    print('[!] Please save your hash in a file, we dont decrypt.')
+    print('[!] Please save your original password in a file, we dont decrypt.')
     print(dots)
 
 def base85e():
-    msg = input('\n[!] Type the message to hash: ')
+    msg = input('\n[!] - Type the message to hash: ')
     msgb = msg.encode('ascii') # enconding to ascii
     base64b = base64.b85encode(msgb) # enconding in base85
     base64m = base64b.decode('ascii') # decoding the ascii
     
     sha_signature = \
-        hashlib.sha384(base64m.encode()).hexdigest()
-    print('\nEncoded key: ' + sha_signature)
+        hashlib.sha384(base64m.encode()).hexdigest() # enconding the hash with sha384
+    print('\nYour hash: ' + sha_signature)
     time.sleep(15)
     return sha_signature
 
